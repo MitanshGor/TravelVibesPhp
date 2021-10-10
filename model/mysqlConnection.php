04ba7d33b209cd611b244e7e1eab572c08ed963d
gormitansh@gmail.com <gormitansh.com>
2021-10-10 15:30:29 +0530

    heroku db added

--- model/mysqlConnection.php
+++ model/mysqlConnection.php
@@ -1,6 +1,18 @@
 <?php
 // Create connection
-$con=mysqli_connect("localhost","root","root","travelvibes"); 
+
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+
+
+$con = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+// $con=mysqli_connect("localhost","root","root","travelvibes"); 
 
 // Check connection
 if (mysqli_connect_errno())
