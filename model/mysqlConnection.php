ae6493f3f774030230015ac1895bf3dc2b0276a4
Mitansh Gor <gormitansh@gmail.com>
2022-09-28 17:07:15 +0530

    database connection updated to azure

--- model/mysqlConnection.php
+++ model/mysqlConnection.php
@@ -1,4 +1,4 @@
-<?php
+<!-- < ?php
 // Create connection
 
 //Get Heroku ClearDB connection information
@@ -23,4 +23,23 @@ if (mysqli_connect_errno())
   else{
     // echo "<h2>MYSQL TravelVibes Database Connected !!!</h2>";
 }
-?> 
+?>  -->
+
+
+
+<?php
+  // PHP Data Objects(PDO) Sample Code:
+  try {
+      $conn = new PDO("sqlsrv:server = tcp:travelvibes.database.windows.net,1433; Database = travelvibes", "Indus_CC_travel", "mitansh@2002");
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  }
+  catch (PDOException $e) {
+      print("Error connecting to SQL Server.");
+      die(print_r($e));
+  }
+
+  // SQL Server Extension Sample Code:
+  $connectionInfo = array("UID" => "Indus_CC_travel", "pwd" => "mitansh@2002", "Database" => "travelvibes", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+  $serverName = "tcp:travelvibes.database.windows.net,1433";
+  $con = sqlsrv_connect($serverName, $connectionInfo);
+?>
